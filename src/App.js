028a5aecb9d56acb2523b179e097d0821d0f0abb
markitoo3 <dietrich.marko@gmail.com>
2022-04-07 17:17:08 +0200

    w12 session

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/button/button'
+const text = "Valami szöveg"
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {text}
+        </p>
+        <p>Másik szöveg</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+const text = "Szevasz"
+
+function App2() {
+  return (
+    <>
+        <div></div>
+        <div className="App2">         
+            <p>
+            {text}
+            </p>           
+        </div>
+    </>
+  );
+}
+
+export default App2;
