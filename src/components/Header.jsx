143b1c47f6fd0300fcc0d0b321b980a8dd5af333
markitoo3 <dietrich.marko@gmail.com>
2022-04-11 17:45:39 +0200

    practice Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react"
+
+class Header extends Component {
+    render() {
+        return (
+            <header>{this.props.title}</header>
+        )
+    }
+}
+
+export default Header
